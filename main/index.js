2ef69974b7029112eb34190c48f3289065f3633d
Sebastian Penafiel <penafiel.sebastian@gmail.com>
2018-12-28 19:00:11 -0500

    clustering

--- main/index.js
+++ main/index.js
@@ -1,17 +1,39 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-function doWork(duration) {
-  const start = Date.now();
-  while(Date.now() - start < duration) {
-    // simulates a certain amount of time, 
-    // to block the event loop
+/**
+ * cluster.isMaster
+ * Will return true if the instance is the cluster manager
+ */
+
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Cause index.js to be executed *again* 
+  // but in child mode
+  // Creates 4 children instances
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+} else { // child instance
+  const express = require('express');
+  const app = express();
+  
+  function doWork(duration) {
+    const start = Date.now();
+    while(Date.now() - start < duration) {
+      // simulates a certain amount of time, 
+      // to block the event loop
+    }
   }
+  
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('hello');
+  });
+
+  app.get('/fast', (req, res) => {
+    res.send('this was fast');
+  });
+  
+  app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-  doWork(5000);
-  res.send('hello');
-});
-
-app.listen(3000);
